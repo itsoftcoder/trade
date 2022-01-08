a6a0d917b97ce1903390630b08b6b2d2158e0faf
itsoftcoder <gembatplc@gmail.com>
2022-01-08 19:57:47 +0600

    change positon

--- resources/views/sale_pos/receipts/classic.blade.php
+++ resources/views/sale_pos/receipts/classic.blade.php
@@ -195,7 +195,7 @@ function numberTowords($num)
 			@endif
 		</div>
 		
-		<div class="col-md-6 bg-primary text-light">
+		<div class="col-md-6 bg-primary text-light mb-3">
 		    <!-- Invoice  number, Date  -->
 			<p style="width: 100% !important; font-size: 14px; background:blue; color:white;" class="word-wrap" style="">
 				<span class="pull-left text-left word-wrap" style="margin-bottom:0px;">
@@ -351,10 +351,159 @@ function numberTowords($num)
 			</p>
 		</div>
 		
-		<div class="col-md-6 bg-primary text-light">
-			@if(!empty($receipt_details->customer_info))
-			<b style="margin-bottom:0px;">{!! $receipt_details->customer_info !!}</b> 
-			@endif
+		<div class="col-md-6 bg-primary text-light mb-3">
+			<p style="width: 100% !important; font-size: 14px; background:blue; color:white;" class="word-wrap" style="">
+				<span class="pull-left text-left word-wrap" style="margin-bottom:0px;">
+					@if(!empty($receipt_details->invoice_no_prefix))
+					<b>{!! $receipt_details->invoice_no_prefix !!}</b>
+					@endif
+					{{$receipt_details->invoice_no}}
+
+					@if(!empty($receipt_details->types_of_service))
+					<br />
+					<span class="pull-left text-left">
+						<strong>{!! $receipt_details->types_of_service_label !!}:</strong>
+						{{$receipt_details->types_of_service}}
+						<!-- Waiter info -->
+						@if(!empty($receipt_details->types_of_service_custom_fields))
+						@foreach($receipt_details->types_of_service_custom_fields as $key => $value)
+						<br><strong>{{$key}}: </strong> {{$value}}
+						@endforeach
+						@endif
+					</span>
+					@endif
+
+					<!-- Table information-->
+					@if(!empty($receipt_details->table_label) || !empty($receipt_details->table))
+					<br />
+					<span class="pull-left text-left" style="font-size: 14px;">
+						@if(!empty($receipt_details->table_label))
+						<b>{!! $receipt_details->table_label !!}</b>
+						@endif
+						{{$receipt_details->table}}
+
+						<!-- Waiter info -->
+					</span>
+					@endif
+
+					<!-- customer info -->
+					@if(!empty($receipt_details->customer_info))
+					<b style="margin-bottom:0px;">{!! $receipt_details->customer_info !!}</b> 
+					@endif
+					@if(!empty($receipt_details->client_id_label))
+					<br />
+					<b>{{ $receipt_details->client_id_label }}</b> {{ $receipt_details->client_id }}
+					@endif
+					@if(!empty($receipt_details->customer_tax_label))
+					<br />
+					<b style="margin-top:0px !important!;">{{ $receipt_details->customer_tax_label }}</b> {{ $receipt_details->customer_tax_number }}
+					@endif
+					@if(!empty($receipt_details->customer_custom_fields))
+					<br />{!! $receipt_details->customer_custom_fields !!}
+					@endif
+					@if(!empty($receipt_details->sales_person_label))
+					<br />
+					<b>{{ $receipt_details->sales_person_label }}</b> {{ $receipt_details->sales_person }}
+					@endif
+					@if(!empty($receipt_details->customer_rp_label))
+					<br />
+					<strong>{{ $receipt_details->customer_rp_label }}</strong> {{ $receipt_details->customer_total_rp }}
+					@endif
+				</span>
+
+				<span class="pull-right text-left" style="font-size: 14px;">
+					{{-- <b>{{$receipt_details->date_label}}</b> {{$receipt_details->invoice_date}} --}}
+
+					@if(!empty($receipt_details->due_date_label))
+					<br><b>{{$receipt_details->due_date_label}}</b> {{$receipt_details->due_date ?? ''}}
+					@endif
+
+					@if(!empty($receipt_details->brand_label) || !empty($receipt_details->repair_brand))
+					<br>
+					@if(!empty($receipt_details->brand_label))
+					<b>{!! $receipt_details->brand_label !!}</b>
+					@endif
+					{{$receipt_details->repair_brand}}
+					@endif
+
+
+					@if(!empty($receipt_details->device_label) || !empty($receipt_details->repair_device))
+					<br>
+					@if(!empty($receipt_details->device_label))
+					<b>{!! $receipt_details->device_label !!}</b>
+					@endif
+					{{$receipt_details->repair_device}}
+					@endif
+
+					@if(!empty($receipt_details->model_no_label) || !empty($receipt_details->repair_model_no))
+					<br>
+					@if(!empty($receipt_details->model_no_label))
+					<b>{!! $receipt_details->model_no_label !!}</b>
+					@endif
+					{{$receipt_details->repair_model_no}}
+					@endif
+
+					@if(!empty($receipt_details->serial_no_label) || !empty($receipt_details->repair_serial_no))
+					<br>
+					@if(!empty($receipt_details->serial_no_label))
+					<b>{!! $receipt_details->serial_no_label !!}</b>
+					@endif
+					{{$receipt_details->repair_serial_no}}<br>
+					@endif
+					@if(!empty($receipt_details->repair_status_label) || !empty($receipt_details->repair_status))
+					@if(!empty($receipt_details->repair_status_label))
+					<b>{!! $receipt_details->repair_status_label !!}</b>
+					@endif
+					{{$receipt_details->repair_status}}<br>
+					@endif
+
+					@if(!empty($receipt_details->repair_warranty_label) || !empty($receipt_details->repair_warranty))
+					@if(!empty($receipt_details->repair_warranty_label))
+					<b>{!! $receipt_details->repair_warranty_label !!}</b>
+					@endif
+					{{$receipt_details->repair_warranty}}
+					<br>
+					@endif
+
+					<!-- Waiter info -->
+					@if(!empty($receipt_details->service_staff_label) || !empty($receipt_details->service_staff))
+					<br />
+					@if(!empty($receipt_details->service_staff_label))
+					<b>{!! $receipt_details->service_staff_label !!}</b>
+					@endif
+					{{$receipt_details->service_staff}}
+					@endif
+					@if(!empty($receipt_details->shipping_custom_field_1_label))
+					<br><strong>{!!$receipt_details->shipping_custom_field_1_label!!} :</strong> {!!$receipt_details->shipping_custom_field_1_value ?? ''!!}
+					@endif
+
+					@if(!empty($receipt_details->shipping_custom_field_2_label))
+					<br><strong>{!!$receipt_details->shipping_custom_field_2_label!!}:</strong> {!!$receipt_details->shipping_custom_field_2_value ?? ''!!}
+					@endif
+
+					@if(!empty($receipt_details->shipping_custom_field_3_label))
+					<br><strong>{!!$receipt_details->shipping_custom_field_3_label!!}:</strong> {!!$receipt_details->shipping_custom_field_3_value ?? ''!!}
+					@endif
+
+					@if(!empty($receipt_details->shipping_custom_field_4_label))
+					<br><strong>{!!$receipt_details->shipping_custom_field_4_label!!}:</strong> {!!$receipt_details->shipping_custom_field_4_value ?? ''!!}
+					@endif
+
+					@if(!empty($receipt_details->shipping_custom_field_5_label))
+					<br><strong>{!!$receipt_details->shipping_custom_field_2_label!!}:</strong> {!!$receipt_details->shipping_custom_field_5_value ?? ''!!}
+					@endif
+					{{-- sale order --}}
+					@if(!empty($receipt_details->sale_orders_invoice_no))
+					<br>
+					<strong>@lang('restaurant.order_no'):</strong> {!!$receipt_details->sale_orders_invoice_no ?? ''!!}
+					@endif
+
+					@if(!empty($receipt_details->sale_orders_invoice_date))
+					<br>
+					<strong>@lang('lang_v1.order_dates'):</strong> {!!$receipt_details->sale_orders_invoice_date ?? ''!!}
+					@endif
+				</span>
+			</p>
 		    <!-- Address -->
 			{{-- <p style="font-size: 16px;border-bottom: 1px solid #d6d6d6"> --}}
 				{{-- @if(!empty($receipt_details->address)) --}}
